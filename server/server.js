73b0d3b72f70ce1b975bc4327ab1666acc99e7a4
Marjanus <martynas.janusevicius@gmail.com>
2017-01-28 20:55:49 +0200

    added Put and Delete to the server.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const express = require('express');
+const router = express.Router();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const dbSettings = require('../dbSettings');
 const List = require('./model/list');
-const router = express.Router();
 const app = express();
 const port = 3001;
 const db = `mongodb://${dbSettings.user}:${dbSettings.pass}@ds117189.mlab.com:17189/shopping-list`;
@@ -33,15 +33,39 @@ router.get('/list', (req, res) => {
 
 router.post('/list', (req, res) => {
 	if (!req.body.name || !req.body.quantity) return res.sendStatus(400);
-	let newList = new List();
-	newList.name = req.body.name;
-	newList.quantity = req.body.quantity;
+	let newItem = new List();
+	newItem.name = req.body.name;
+	newItem.quantity = req.body.quantity;
 	
-	newList.save(function(err, book){
+	newItem.save((err, item) => {
 		if(err) res.send(err);
 		res.send("Item was added to the list"); 
 	});
 })
+
+router.put('/list/:id', (req, res) => {
+		List.findById(req.params.id, (err, item) => {
+			if(err) res.send(err);
+			if(req.body.name) item.name = req.body.name;
+			if(req.body.quantity) item.quantity = req.body.quantity;
+			item.save((err, item) => {
+				if(err) res.send(err);
+				res.send('Item was updated');
+			});	
+		});
+	});
+	
+router.delete('/list/:id', (req, res) => {
+		let id = req.params.id;
+		List.findById(id, (err,item) => {
+			if(err) res.send(err);
+			List.remove({_id: id}, err => {
+				if (err) res.send(err);
+				res.send('Item was deleted');
+			});
+		});
+	});
+
 
 app.use('/api', router);
 
